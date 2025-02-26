d06f541874d96a668d160d7aadb8024ac5e7d118
Paul Hovley <rphovley@gmail.com>
2025-02-26 16:41:43 -0700

    provide firebase as config

---
+++ src/lib/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBc4ua1PwrCB_68qbfyw1ophwNFHzF9XxM",
+  authDomain: "ebb-roadmap.firebaseapp.com",
+  projectId: "ebb-roadmap",
+  storageBucket: "ebb-roadmap.firebasestorage.app",
+  messagingSenderId: "198131675100",
+  appId: "1:198131675100:web:081de5f8dd812abf82a919"
+}; 
